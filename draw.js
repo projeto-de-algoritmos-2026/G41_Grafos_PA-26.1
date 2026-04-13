5d98c98988404d300ea2b4edb0188dd0360fad59
caio-venancio <caio.venancio784@gmail.com>
2026-04-12 23:05:58 -0300

    feat: adicionar renderização de setas

---
+++ draw.js
@@ -0,0 +1,61 @@
+export function drawGrid(rows, cols, cellSize, ctx){
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      const x = j * cellSize;
+      const y = i * cellSize;
+
+      ctx.strokeRect(x, y, cellSize, cellSize);
+    }
+  }
+}
+
+export function drawArrows(ctx, grid, cellSize) {
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      if (!grid[i][j].active) continue;
+      drawArrow(ctx, i, j, grid[i][j].direction, cellSize);
+    }
+  }
+}
+
+function drawArrow(ctx, i, j, direction, cellSize) {
+  const x = j * cellSize;
+  const y = i * cellSize;
+
+  ctx.save();
+
+  // move para o centro da célula
+  ctx.translate(x + cellSize / 2, y + cellSize / 2);
+
+  // define rotação
+  let angle = 0;
+
+  if (direction === "RIGHT") angle = 0;
+  if (direction === "DOWN") angle = Math.PI / 2;
+  if (direction === "LEFT") angle = Math.PI;
+  if (direction === "UP") angle = -Math.PI / 2;
+
+  ctx.rotate(angle);
+
+  // desenha no centro
+  drawArrowShape(ctx, cellSize);
+
+  ctx.restore();
+}
+
+export function drawArrowShape(ctx, size) {
+  const center = size / 2;
+
+  ctx.beginPath();
+
+  // corpo (linha)
+  ctx.moveTo(-center / 2, 0);
+  ctx.lineTo(center / 2, 0);
+
+  // ponta (triângulo)
+  ctx.lineTo(center / 4, -center / 4);
+  ctx.moveTo(center / 2, 0);
+  ctx.lineTo(center / 4, center / 4);
+
+  ctx.stroke();
+}
--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 console.log("Script.js iniciado.")
 
+import { drawGrid, drawArrows } from './draw.js'
+import { createRandomGrid } from './utilities.js';
+
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
@@ -8,22 +11,13 @@ canvas.height = 400;
 const rows = 5
 const cols = 5
 const cellSize = 80
-
-function drawGrid(rows, cols, cellSize, ctx){
-  for (let i = 0; i < rows; i++) {
-    for (let j = 0; j < cols; j++) {
-      const x = j * cellSize;
-      const y = i * cellSize;
-
-      ctx.strokeRect(x, y, cellSize, cellSize);
-    }
-  }
-}
+const grid = createRandomGrid(rows, cols)
 
 function loop(){
     ctx.clearRect(0, 0, canvas.width, canvas.height); // Limpa canvas
 
     drawGrid(rows, cols, cellSize, ctx);
+    drawArrows(ctx, grid, cellSize);
 
     requestAnimationFrame(loop);
 }
